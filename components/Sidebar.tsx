4d1b37f07a2abe032b2f2bd16aee3f202b1666cc
kouhei-github <kohei0801nagamatsu@gmail.com>
2023-12-06 10:53:38 +0900

    レスポンシブ・サイドバーとダッシュボード・レイアウトのUIを更新
    ダッシュボードとサイドバーコンポーネントのCSSクラスを調整し、レスポンシブレイアウトに対応。モバイルとの互換性を向上させるため、フロントエンドに変更を加えました。ダッシュボードの高さを100vhに、サイドバーの幅を70%に変更。モバイルナビゲーション用にサイドバーに "X "クローズボタンを追加し、一部のラベルをモバイルビュー用に "Help & Information "の代わりに "Help "と短くし、利用可能なスペース内に要素を収まるようにした。

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import Accordion from '@/components/Accordion'
 import {useContext} from 'react'
 import {SideBarContext} from '@/contexts/sideBar'
+import {IoCloseSharp} from 'react-icons/io5'
 
 type Props = {
   logoImage: string
@@ -19,11 +20,15 @@ type Props = {
 export const Sidebar = (props: Props) => {
   const sidebarCtx = useContext(SideBarContext)
   return (
-    <div className={` h-[95vh] ease-in-out duration-[600ms] ${sidebarCtx.open ? "w-[25%] border-r border-[#B3B3B3] px-10 pt-7" : "w-[0%]"}`}>
+    <div className={`bg-black z-20 md:h-[95vh] ease-in-out duration-[600ms] ${sidebarCtx.open ? "md:w-[25%] w-[70%] h-[100vh] md:relative fixed top-0 border-r border-[#B3B3B3] md:px-10 px-5 pt-3 md:pt-7" : "w-[0%]"}`}>
       <Accordion>
         <div className={"flex items-center space-x-4 mb-10"}>
           <Image src={props.logoImage} width={60} height={60} alt={"ロゴ"} className={"object-cover rounded-full"} />
           {sidebarCtx.open ? <p className={" font-bold text-lg"}>{props.adminName}</p> : <></>}
+        </div>
+
+        <div className={`fixed cursor-pointer block md:hidden ${sidebarCtx.open ? "-top-2 -right-2" : "-top-52"}`} onClick={() => sidebarCtx.setOpen(!sidebarCtx.open)}>
+          <IoCloseSharp size={25} color={"white"} />
         </div>
 
         <div className={"flex items-center space-x-4 mb-8"}>
@@ -46,7 +51,7 @@ export const Sidebar = (props: Props) => {
           <div className={"h-[10vh] text-[#8F8F8F]"}>
             <Link href={"/logout"} className={`flex items-center space-x-3 py-4 px-4 rounded-2xl cursor-pointer`}>
               <IoMdHelp size={25} color={"white"} />
-              <p className={""}>Help & Information</p>
+              <p className={""}>Help</p>
             </Link>
 
             <Link href={"/help"} className={`flex items-center space-x-3 py-4 px-4 rounded-2xl cursor-pointer`}>
