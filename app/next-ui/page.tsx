d386b641b0cff267b34a8d74c1508b6cbfe6ea37
kouhei-github <kohei0801nagamatsu@gmail.com>
2023-12-07 21:09:24 +0900

    電卓アプリケーション機能の実装
    page.tsxの計算ロジックやhooks/electronicの新しいcalculateHookの作成など、電卓アプリケーションの基本機能を追加しました。これらの機能により、電卓は基本的な算術計算を行うことができる。UX/UIを向上させるために、scssファイルでスタイル調整が行われ、特定の要素でポインタイベントが無効になりました。Tailwindの設定も更新された。

--- app/next-ui/page.tsx
+++ app/next-ui/page.tsx
@@ -1,7 +1,44 @@
 "use client"
 import "../styles/_culc_app.scss"
+import {useState} from 'react'
+
+type logic = "-" | "+" | "x" | "÷" | "=" | "c" | "nothing"
 
 export default function Home() {
+
+  const [typing, setTyping] = useState<string>("")
+
+  const [evaluate, setEvaluate] = useState<string>("")
+
+  const[answer, setAnswer] = useState<number>(0)
+  const callCalc = (logic: logic) => {
+    if (logic === "c")  {
+      setTyping("")
+      setAnswer(0)
+      setEvaluate("")
+      return
+    }
+    let func = evaluate + typing + logic
+
+    setTyping("")
+
+    if(func.includes("x") || func.includes("÷")){
+      func ="(" + func
+    }
+
+    const result = func.replaceAll("x", ")*").replaceAll("÷", ")/").replaceAll("nothing", "").replaceAll("=", "")
+    if (logic === "="){
+      setTyping("")
+      setAnswer(eval(result))
+      return
+    }
+
+    setEvaluate(result)
+    setAnswer(eval(result.slice(0,-1)))
+
+  }
+
+
   return (
       <div className={""}>
         <div className="avatron">
@@ -11,7 +48,7 @@ export default function Home() {
             <div className="stripe"></div>
           </div>
           <div className="result">
-
+          <p className={"text-2xl text-white"}>{answer}</p>
           </div>
           <div className="button-container">
             <div className="logo-container">
@@ -21,7 +58,7 @@ export default function Home() {
             </div>
             <div className="button-container-top">
               <div className="button-top-row">
-                <div className="button button--orange button-first">
+                <div onClick={() => callCalc("c")} className="button button--orange button-first">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text">C</div>
@@ -57,25 +94,25 @@ export default function Home() {
                 </div>
               </div>
               <div className="button-row second-row">
-                <div className="button button--gray button-first">
+                <div onClick={() => setTyping(typing+"7")} className="button button--gray button-first">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number">7</div>
                   </div>
                 </div>
-                <div className="button button--gray button-second">
+                <div onClick={() => setTyping(typing+"8")} className="button button--gray button-second">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number">8</div>
                   </div>
                 </div>
-                <div className="button button--gray button-third">
+                <div onClick={() => setTyping(typing+"9")} className="button button--gray button-third">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number ">9</div>
                   </div>
                 </div>
-                <div className="button button--white button-fourth">
+                <div onClick={() => callCalc("-")} className="button button--white button-fourth">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number button-text--bold">-</div>
@@ -83,25 +120,25 @@ export default function Home() {
                 </div>
               </div>
               <div className="button-row third-row">
-                <div className="button button--gray button-first">
+                <div onClick={() => setTyping(typing+"4")} className="button button--gray button-first">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number">4</div>
                   </div>
                 </div>
-                <div className="button button--gray button-second">
+                <div onClick={() => setTyping(typing+"5")} className="button button--gray button-second">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number">5</div>
                   </div>
                 </div>
-                <div className="button button--gray button-third">
+                <div onClick={() => setTyping(typing+"6")} className="button button--gray button-third">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number ">6</div>
                   </div>
                 </div>
-                <div className="button button--white button-fourth">
+                <div onClick={() => callCalc("÷")} className="button button--white button-fourth">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number button-text--bold">÷</div>
@@ -109,25 +146,25 @@ export default function Home() {
                 </div>
               </div>
               <div className="button-row fourth-row">
-                <div className="button button--gray button-first">
+                <div onClick={() => setTyping(typing+"1")} className="button button--gray button-first">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number">1</div>
                   </div>
                 </div>
-                <div className="button button--gray button-second">
+                <div onClick={() => setTyping(typing+"2")} className="button button--gray button-second">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number">2</div>
                   </div>
                 </div>
-                <div className="button button--gray button-third">
+                <div onClick={() => setTyping(typing+"3")} className="button button--gray button-third">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number">3</div>
                   </div>
                 </div>
-                <div className="button button--white button-fourth">
+                <div onClick={() => callCalc("x")} className="button button--white button-fourth">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number button-text--bold">x</div>
@@ -135,7 +172,7 @@ export default function Home() {
                 </div>
               </div>
               <div className="button-row fifth-row">
-                <div className="button button--gray button-first">
+                <div onClick={() => setTyping(typing+"0")} className="button button--gray button-first">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number">0</div>
@@ -147,13 +184,13 @@ export default function Home() {
                     <div className="button-text button-text--number">.</div>
                   </div>
                 </div>
-                <div className="button button--white button-fourth">
+                <div onClick={() => callCalc("=")} className="button button--white button-fourth">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number ">=</div>
                   </div>
                 </div>
-                <div className="button button--white button-fourth">
+                <div onClick={() => callCalc("+")} className="button button--white button-fourth">
                   <div className="shading"></div>
                   <div className="button-inner">
                     <div className="button-text button-text--number button-text--bold">+</div>
